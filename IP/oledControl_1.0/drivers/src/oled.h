f5e393c032b8ed12ae38a406429cfb830eccd41f
kunalsanwlecha <kunalsanwlecha992@gmail.com>
2026-07-01 17:10:37 +0530

    Create oled.h

---
+++ IP/oledControl_1.0/drivers/src/oled.h
@@ -0,0 +1,16 @@
+#ifndef SRC_OLED_H_
+#define SRC_OLED_H_
+
+#include<xil_types.h>
+
+typedef struct oledControl{
+	u32 baseAddress;
+}oledControl;
+
+int initOled(oledControl *myOled,u32 baseAddress);
+void writeCharOled(oledControl *myOled,char myChar);
+void printOled(oledControl *myOled,char *myString);
+void clearOled(oledControl *myOled);
+
+
+#endif 
